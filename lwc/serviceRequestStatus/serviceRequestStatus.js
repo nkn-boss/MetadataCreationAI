05943e59682e6680bb5fb2fa03666c02e366fe7d
nkn-boss <123087650+nkn-boss@users.noreply.github.com>
2025-01-20 20:09:30 +0530

    Adding lwc/serviceRequestStatus/serviceRequestStatus.js for issue AISDLC-881

---
+++ lwc/serviceRequestStatus/serviceRequestStatus.js
@@ -0,0 +1,5 @@
+import { LightningElement, api } from 'lwc';
+export default class ServiceRequestStatus extends LightningElement {
+    @api recordId;
+    // JavaScript content for status display
+}
